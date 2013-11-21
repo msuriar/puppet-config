b86a82754b687da68ee3a50cd37cbc028c3233af
Murali Suriar <code@suriar.com>
2013-11-21 02:33:59 +0000

    Initial attempt at bootstrapping CA based login.

---
+++ modules/ssh_ca_cfg/manifests/init.pp
@@ -0,0 +1,40 @@
+class ssh_ca_cfg  {
+
+  package { 'sudo':
+    ensure => installed,
+  }
+
+  file { '/etc/sudoers':
+    ensure  => file,
+    require => Package['sudo'],
+    source  => "puppet:///modules/ssh_ca_cfg/sudoers",
+  }
+
+  user { 'muz':
+    ensure         => present,
+    groups         => ["sudo"],
+    membership     => minimum,
+    require        => Package['sudo'],
+    managehome     => true,
+    password       => '*',
+  }
+
+  package { 'ssh':
+    ensure => installed,
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => file,
+    require => [ Package['ssh'], User['muz'] ],
+    source  => "puppet:///modules/ssh_ca_cfg/sshd_config",
+    notify  => Service['ssh'],
+  }
+
+  service { 'ssh' : }
+
+  file { '/etc/ssh/ca.suriar.net.pub':
+    ensure  => file,
+    require => Package['ssh'],
+    source  => "puppet:///modules/ssh_ca_cfg/ca.suriar.net.pub",
+  }
+}
