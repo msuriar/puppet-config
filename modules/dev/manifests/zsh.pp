ba3ac891ede4ce330e77b7fb968b1ea9cded758a
Murali Suriar <code@suriar.com>
2014-01-10 21:32:54 +0000

    zsh package and user shell managed in base module.

--- modules/dev/manifests/zsh.pp
+++ modules/dev/manifests/zsh.pp
@@ -1,37 +1,24 @@
-class dev::zsh {
-
-  include dev::zsh::config
-
-  package {'zsh':
-    ensure => latest,
+class dev::zsh::config {
+  $user = 'muz'
+  include dotfiles
+  File {
+    owner => $user,
+    group => $user,
+    mode  => 0644,
   }
 
-  class dev::zsh::config {
-    $user = 'muz'
-    include dotfiles
-    File {
-      owner => $user,
-      group => $user,
-      mode  => 0644,
-    }
+  file {"/home/$user/.zshrc":
+    ensure => link,
+    target => '.dotfiles/zsh/zshrc',
+  }
 
-    User[$user] {
-      shell => '/bin/zsh',
-    }
+  file {"/home/$user/.zshenv":
+    ensure => link,
+    target => '.dotfiles/zsh/zshenv',
+  }
 
-    file {"/home/$user/.zshrc":
-      ensure => link,
-      target => '.dotfiles/zsh/zshrc',
-    }
-
-    file {"/home/$user/.zshenv":
-      ensure => link,
-      target => '.dotfiles/zsh/zshenv',
-    }
-
-    file {"/home/$user/.zprofile":
-      ensure => link,
-      target => '.dotfiles/zsh/zprofile',
-    }
+  file {"/home/$user/.zprofile":
+    ensure => link,
+    target => '.dotfiles/zsh/zprofile',
   }
 }
